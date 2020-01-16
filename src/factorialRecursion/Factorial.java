637c62f4f51d1567fe51a15aee99bf0ba72bc65c
sidhantdubey <sidhant.dubey_cs18@gla.ac.in>
2020-01-16 10:48:24 +0530

    added more documentation

--- src/factorialRecursion/Factorial.java
+++ src/factorialRecursion/Factorial.java
@@ -2,19 +2,31 @@ package factorialRecursion;
 
 import java.util.Scanner;
 
+/**
+ * program to calculate factorial using recursive call
+ */
 public class Factorial {
+    /**
+     * main method
+     *
+     * @param args to take commandline arguments
+     */
     public static void main(String[] args) {
-    Scanner in=new Scanner(System.in);
-    double number=in.nextDouble();
-    System.out.println(factoriall(number));
+        Scanner in = new Scanner(System.in);
+        double number = in.nextDouble();
+        System.out.println(factoriall(number));
 
-}
+    }
 
+    /**
+     * methos to calculate factorial using recursive calling
+     * @param number the nuber whose factorial is to be calculated
+     * @return number * number-1*...*1 using recursive call
+     */
     public static double factoriall(double number){
         if(number==1){
             return number;
-        }
-        else{
+        } else{
             return number* factoriall(number-1);
         }
     }
