64a2135425e61d295c781ba8cb46d6dc0c9753a8
sidhantdubey <sidhant.dubey_cs18@gla.ac.in>
2020-01-16 10:48:08 +0530

    recursive Factorial call

--- src/factorialRecursion/Factorial.java
+++ src/factorialRecursion/Factorial.java
@@ -3,4 +3,19 @@ package factorialRecursion;
 import java.util.Scanner;
 
 public class Factorial {
+    public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+    double number=in.nextDouble();
+    System.out.println(factoriall(number));
+
+}
+
+    public static double factoriall(double number){
+        if(number==1){
+            return number;
+        }
+        else{
+            return number* factoriall(number-1);
+        }
+    }
 }
