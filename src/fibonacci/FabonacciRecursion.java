e61d08539e2cb7a99944edf9b3183df0addf581d
sidhantdubey <sidhant.dubey_cs18@gla.ac.in>
2020-01-16 11:12:11 +0530

    added FabonacciRecursion logic other than teacher

--- src/fibonacci/FabonacciRecursion.java
+++ src/fibonacci/FabonacciRecursion.java
@@ -1,8 +1,22 @@
 package fibonacci;
 
+import java.util.Scanner;
+
 public class FabonacciRecursion {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int number = in.nextInt();//number of terms
+        int a = 0;
+        int b = 1;
+        fabonacci(a, b, number);
+    }
 
-
+    public static int fabonacci(int a, int b, int number) {
+        System.out.print(a + " ");
+        if (number == 0) {
+            return (b);
+        } else {
+            return fabonacci(a + b, a, number - 1);
+        }
     }
 }
