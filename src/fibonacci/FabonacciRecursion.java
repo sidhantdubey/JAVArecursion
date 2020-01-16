d87c492f7e80b377e367f75d83a3010328d79c71
yashsinghcs <yash.singh_cs18@gla.glc.in>
2020-01-16 16:35:39 +0530

    added some documentation!!

--- src/fibonacci/FabonacciRecursion.java
+++ src/fibonacci/FabonacciRecursion.java
@@ -6,6 +6,11 @@ import java.util.Scanner;
  * @author YASH kUMAR SINGH
  */
 public class FabonacciRecursion {
+    /**
+     * main method
+     *
+     * @param args to take command line arguments but not req as in here
+     */
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int number = in.nextInt();//number of terms
@@ -14,6 +19,13 @@ public class FabonacciRecursion {
         fabonacci(a, b, number);
     }
 
+    /**
+     * this function recursively calls itself  and prints fabonacci series
+     *
+     * @param a      first number is sequence
+     * @param b      second number in the sequence
+     * @param number no to times loops/recursion to be executed and no of terms printed
+     */
     public static void fabonacci(int a, int b, int number) {
         System.out.print(a + " ");
         if (number == 1) {
